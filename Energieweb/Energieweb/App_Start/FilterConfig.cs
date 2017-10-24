c07e50aa85364dc0e796a83c572a1613633b7edb
Bas <b.d.r._bas@live.nl>
2017-10-24 12:11:30 +0200

    created new project with correct setttings

---
+++ Energieweb/Energieweb/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Energieweb
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}

e39aa1145aa53d5acac04f59b0afbe3d26dea875
lightnessking <moostebring@hotmail.com>
2017-10-25 13:02:58 +0200

    init

--- Energieweb/Energieweb/Controllers/HomeController.cs
+++ Energieweb/Energieweb/Controllers/HomeController.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data;
+using System.Configuration;
+using MySql.Data.MySqlClient;
 
 namespace Energieweb.Controllers
 {
     public class HomeController : Controller
     {
+        MySql.Data.MySqlClient.MySqlConnection conn;
+        MySql.Data.MySqlClient.MySqlCommand cmd;
+        MySql.Data.MySqlClient.MySqlDataReader reader;
+        string query;
+        string apparaat = "";
         public ActionResult Index()
         {
             return View();
@@ -15,8 +23,30 @@ namespace Energieweb.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
+            System.Diagnostics.Debug.WriteLine("asdfasdfasdfasdf");
+            String connString = System.Configuration.ConfigurationManager.ConnectionStrings["constr"].ToString();
+            conn = new MySql.Data.MySqlClient.MySqlConnection(connString);
+            conn.Open();
+            query = "";
+            ///query = "INSERT INTO `apparaat`(`id`, `naam`, `max`, `merk`, `type_fk`) VALUES (10,'theelepel',10,'lepelBV',1)";
+            query = "SELECT * FROM apparaat;";
+            cmd = new MySql.Data.MySqlClient.MySqlCommand(query, conn);
+            
+            reader = cmd.ExecuteReader();
+            
+            conn.Close();
+            
+            while(reader.HasRows && reader.Read())
+            {
+                apparaat = apparaat + reader["naam"] + " ";
+                ViewBag.Test = reader.FieldCount;
+            }
+            
+            System.Diagnostics.Debug.WriteLine("resultaat:" + apparaat.ToString());
+            System.Diagnostics.Debug.WriteLine("asdfasdfasdfasdf");
+            ViewBag.Message = "test" + apparaat.ToString();
+            ViewBag.Test = apparaat;
+            reader.Close();
             return View();
         }
 
